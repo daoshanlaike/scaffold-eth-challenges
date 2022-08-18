2e155d9bfb8ec9a464f70d283705a1de7317fe6a
daoshanlaike <13968952440@163.com>
2022-08-18 09:53:57 +0800

    Update App.jsx

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -581,7 +581,7 @@ function App(props) {
               <Button
                 type={balanceStaked ? "success" : "primary"}
                 onClick={() => {
-                  tx(writeContracts.Staker.stake({ value: ethers.utils.parseEther("0.5") }));
+                  tx(writeContracts.Staker.stake({ value: ethers.utils.parseEther("0.5") });
                 }}
               >
                 🥩 Stake 0.5 ether!
